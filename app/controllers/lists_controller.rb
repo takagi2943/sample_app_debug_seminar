c6b45c2d358ac6e5c344ba93ecce42d5f2531fea
takagi2943 <154996725+takagi2943@users.noreply.github.com>
2024-01-16 13:13:50 +0000

    デバッグツールを学ぼう【imageタグ削除実行】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,11 +2,11 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
-  
+
   def new
     @list = List.new
   end
-  
+
   def create
     @list = List.new(list_params)
     if @list.save
